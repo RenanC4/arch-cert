3b6e58dce92b5810809406b617c918ed1ac19907
Renan Campos <renanc433@gmail.com>
2021-01-04 14:00:59 -0300

    first commit

---
+++ Aula1/node-api/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const hostname = '172.31.56.78'; // Make sure to change this to your EC2 private IP
+const port = 3000;
+const server = http.createServer((req, res) => {
+ res.statusCode = 200;
+ res.setHeader('Content-Type', 'text/plain');
+ res.end('Hello World from BackSpace Academy!');
+});
+server.listen(port, hostname, () => {
+ console.log(`Server running at http://${hostname}:${port}/`);
+});
